2c74f35ad3ed9530dcf00a68bc6c86a08095eda8
realvuk <102806047+realvuk@users.noreply.github.com>
2025-09-13 16:51:11 +0200

    a

---
+++ Day_03/Day_03.py
@@ -0,0 +1,4 @@
+print("Welcome to Treasute Island. \nYour mission is to find the treasure. \nYou're at a cross road. Where do you want to go?")
+side = input("Type 'left' or 'right' ")
+
+if side = "left"
